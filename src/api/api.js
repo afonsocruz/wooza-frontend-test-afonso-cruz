e0f3d1f84c3c9c2b0eb38117a50bf518b901db48
Afonso <afonsocruz.rj@gmail.com>
2021-02-04 23:28:49 -0300

    removed 'plataformas' from api request

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://private-59658d-celulardireto2017.apiary-mock.com/plataformas',
+  baseURL: 'https://private-59658d-celulardireto2017.apiary-mock.com/',
 });
 
 export default api;
